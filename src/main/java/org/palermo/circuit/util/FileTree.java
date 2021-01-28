7ed1203544ebc3cf74cef3fdf8bc0652f5f3b2e6
unknown <edupalermo@gmail.com>
2021-01-28 05:07:45 +0100

    Version

--- src/main/java/org/palermo/circuit/util/FileTree.java
+++ src/main/java/org/palermo/circuit/util/FileTree.java
@@ -14,7 +14,7 @@ public class FileTree<T> {
 
     public FileTree(File file, Comparator<T> comparator, Converter converter) {
         try {
-            this.raf = new RandomAccessFile(file, "rd");
+            this.raf = new RandomAccessFile(file, "rw");
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
@@ -22,13 +22,13 @@ public class FileTree<T> {
         this.converter = converter;
     }
 
-    public synchronized boolean exists(T item) {
+    public synchronized boolean contains(T item) {
         try {
             if (this.raf.length() == 0) {
                 return false;
             }
 
-            long offset = 0;
+            long offset = getRootOffset();
             byte[] buffer = new byte[converter.getSize()];
 
             do {
@@ -50,18 +50,25 @@ public class FileTree<T> {
         }
     }
 
+    private long getRootOffset() throws IOException {
+        this.raf.seek(Long.BYTES);
+        return this.raf.readLong();
+    }
+
     public synchronized boolean add(T item) {
 
         try {
             if (this.raf.length() == 0) {
-                this.raf.writeLong(0); // quantity of elements
+                this.raf.writeLong(1); // quantity of elements
+                this.raf.writeLong(2 * Long.BYTES); // offset of the root
                 this.raf.write(converter.serialize(item));
                 this.raf.writeLong(0x00);
                 this.raf.writeLong(0x00);
                 return true;
             }
             else {
-                boolean added = add(Long.BYTES, item);
+                this.raf.seek(Long.BYTES);
+                boolean added = add(this.raf.readLong(), item);
 
                 if (added) {
                     this.raf.seek(0);
@@ -107,4 +114,201 @@ public class FileTree<T> {
 
         return true;
     }
+
+    protected int getHeight() {
+        try {
+            if (this.raf.length() == 0) {
+                return 0;
+            }
+            this.raf.seek(Long.BYTES);
+            return getHeight(this.raf.readLong());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int getHeight(long offset) throws IOException {
+        if (offset == 0x00) {
+            return 0;
+        }
+        else {
+            this.raf.seek(offset);
+            this.raf.skipBytes(converter.getSize());
+            long leftOffset = this.raf.readLong();
+            long rightOffset = this.raf.readLong();
+            return Math.max(getHeight(leftOffset), getHeight(rightOffset)) + 1;
+        }
+    }
+    public long getSize() {
+        try {
+            if (this.raf.length() == 0) {
+                return 0;
+            }
+            this.raf.seek(0);
+            return this.raf.readLong();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void reBalance() {
+        try {
+            if (this.raf.length() > 0) {
+                this.raf.seek(Long.BYTES);
+                this.walk(Long.BYTES, this.raf.readLong());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void walk(long position, long offset) throws IOException {
+
+        this.raf.seek(offset);
+        this.raf.skipBytes(converter.getSize());
+        long leftOffset = this.raf.readLong();
+        long rightOffset = this.raf.readLong();
+
+        if (leftOffset != 0x00) {
+            walk(position + converter.getSize(), leftOffset);
+        }
+        if (rightOffset != 0x00) {
+            walk(position + converter.getSize() + Long.BYTES, rightOffset);
+        }
+
+        this.reBalance(position, offset);
+    }
+
+    private void reBalance(long position, long offset) throws IOException {
+
+        this.raf.seek(offset);
+        this.raf.skipBytes(converter.getSize());
+        long leftOffset = this.raf.readLong();
+        long rightOffset = this.raf.readLong();
+
+
+        int balanceFactor = getBalanceFactor(offset);
+
+        if (balanceFactor == 2) {
+            if (getBalanceFactor(rightOffset) >= 0) {
+                this.rotateRightRight(position, offset);
+            }
+            else {
+                this.rotateRightLeft(position, offset);
+            }
+
+        } else if (balanceFactor == -2) {
+            if (getBalanceFactor(leftOffset) <= 0) {
+                this.rotateLeftLeft(position, offset);
+            }
+            else {
+                this.rotateLeftRight(position, offset);
+            }
+        }
+
+        if (Math.abs(getBalanceFactor(offset)) >= 2) {
+            throw new RuntimeException("Fail to balance");
+        }
+    }
+
+    private int getBalanceFactor(long offset) throws IOException {
+        this.raf.seek(offset);
+        this.raf.skipBytes(converter.getSize());
+        long leftOffset = this.raf.readLong();
+        long rightOffset = this.raf.readLong();
+
+        return getHeight(rightOffset) - getHeight(leftOffset);
+    }
+
+    private void rotateRightRight(long parentPosition, long offset) throws IOException {
+        this.raf.seek(offset);
+        this.raf.skipBytes(converter.getSize() + Long.BYTES);
+        long rightOffset = this.raf.readLong();
+        long rightPosition = offset + converter.getSize() + Long.BYTES;
+
+        this.raf.seek(rightOffset);
+        this.raf.skipBytes(converter.getSize());
+        long childLeftOffset = this.raf.readLong();
+        long childLeftPosition = rightOffset + converter.getSize();
+
+        setPositionValue(parentPosition, rightOffset);
+        setPositionValue(childLeftPosition, offset);
+        setPositionValue(rightPosition, childLeftOffset);
+    }
+
+    private void rotateRightLeft(long parentPosition, long offset) throws IOException {
+        System.out.println("rotateRightLeft");
+
+        this.raf.seek(offset);
+        this.raf.skipBytes(converter.getSize() + Long.BYTES);
+        long rightOffset = this.raf.readLong();
+        long rightPosition = offset + converter.getSize() + Long.BYTES;
+
+        this.raf.seek(rightOffset);
+        this.raf.skipBytes(converter.getSize());
+        long rightLeftOffset = this.raf.readLong();
+        long rightLeftPosition = rightOffset + converter.getSize();
+
+        this.raf.seek(rightLeftOffset);
+        this.raf.skipBytes(converter.getSize());
+        long rightLeftLeftOffset = this.raf.readLong();
+        long rightLeftRightOffset = this.raf.readLong();
+        long rightLeftLeftPosition = rightLeftOffset + converter.getSize();
+        long rightLeftRightPosition = rightLeftOffset + converter.getSize() + Long.BYTES;
+
+        setPositionValue(parentPosition, rightLeftOffset);
+        setPositionValue(rightLeftLeftPosition, offset);
+        setPositionValue(rightLeftRightPosition, rightOffset);
+        setPositionValue(rightPosition, rightLeftLeftOffset);
+        setPositionValue(rightLeftPosition, rightLeftRightOffset);
+    }
+    private void rotateLeftLeft(long parentPosition, long offset) throws IOException {
+        this.raf.seek(offset);
+        this.raf.skipBytes(converter.getSize());
+
+        long leftOffset = this.raf.readLong();
+        long leftPosition = offset + converter.getSize();
+
+        this.raf.seek(leftOffset);
+        this.raf.skipBytes(converter.getSize());
+        this.raf.skipBytes(Long.BYTES);
+        long childRightOffset = this.raf.readLong();
+        long childRightPosition = leftOffset + converter.getSize() + Long.BYTES;
+
+        setPositionValue(parentPosition, leftOffset);
+        setPositionValue(childRightPosition, offset);
+        setPositionValue(leftPosition, childRightOffset);
+    }
+    private void rotateLeftRight(long parentPosition, long offset) throws IOException {
+        System.out.println("rotateLeftRight");
+
+        this.raf.seek(offset);
+        this.raf.skipBytes(converter.getSize());
+        long leftOffset = this.raf.readLong();
+        long leftPosition = offset + converter.getSize();
+
+        this.raf.seek(leftOffset);
+        this.raf.skipBytes(converter.getSize() + Long.BYTES);
+        long leftRightOffset = this.raf.readLong();
+        long leftRightPosition = leftOffset + converter.getSize() + Long.BYTES;
+
+        this.raf.seek(leftRightOffset);
+        this.raf.skipBytes(converter.getSize());
+        long leftRightLeftOffset = this.raf.readLong();
+        long leftRightRightOffset = this.raf.readLong();
+        long leftRightLeftPosition = leftRightOffset + converter.getSize();
+        long leftRightRightPosition = leftRightOffset + converter.getSize() + Long.BYTES;
+
+        setPositionValue(parentPosition, leftRightOffset);
+        setPositionValue(leftRightLeftPosition, offset);
+        setPositionValue(leftRightRightPosition, leftOffset);
+        setPositionValue(leftPosition, leftRightLeftOffset);
+        setPositionValue(leftRightPosition, leftRightRightOffset);
+    }
+
+    private void setPositionValue(long position, long offset) throws IOException {
+        this.raf.seek(position);
+        this.raf.writeLong(offset);
+    }
+
 }
---
+++ src/test/java/org/palermo/circuit/util/FileTreeTest.java
@@ -0,0 +1,150 @@
+package org.palermo.circuit.util;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.palermo.circuit.converter.CharConverter;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FileTreeTest {
+
+    @Test
+    void testTree() throws IOException {
+
+        Comparator<Long> comparator = new Comparator<Long>() {
+
+            @Override
+            public int compare(Long o1, Long o2) {
+                return Long.compare(o1, o2);
+            }
+        };
+
+        Converter<Long> converter = new Converter<Long>() {
+
+            @Override
+            public byte[] serialize(Long input) {
+                ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+                buffer.putLong(input);
+                return buffer.array();
+            }
+
+            @Override
+            public Long deserialize(byte[] input) {
+                ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+                buffer.put(input);
+                buffer.flip();//need flip
+                return buffer.getLong();
+            }
+
+            @Override
+            public int getSize() {
+                return Long.BYTES;
+            }
+        };
+
+        FileTree<Long> fileTree = new FileTree<Long>(File.createTempFile("___", ".tmp"), comparator, converter);
+        assertThat(fileTree.getSize()).isEqualTo(0);
+
+        System.out.println(fileTree.getHeight());
+
+        fileTree.add(0L);
+        assertThat(fileTree.getSize()).isEqualTo(1);
+        assertThat(fileTree.contains(0L)).isEqualTo(true);
+        assertThat(fileTree.contains(1L)).isEqualTo(false);
+
+        System.out.println(fileTree.getHeight());
+
+        fileTree.add(1L);
+        assertThat(fileTree.getSize()).isEqualTo(2);
+        assertThat(fileTree.contains(0L)).isEqualTo(true);
+        assertThat(fileTree.contains(1L)).isEqualTo(true);
+        assertThat(fileTree.contains(2L)).isEqualTo(false);
+
+        System.out.println(fileTree.getHeight());
+
+        fileTree.add(2L);
+        assertThat(fileTree.getSize()).isEqualTo(3);
+        assertThat(fileTree.contains(0L)).isEqualTo(true);
+        assertThat(fileTree.contains(1L)).isEqualTo(true);
+        assertThat(fileTree.contains(2L)).isEqualTo(true);
+        assertThat(fileTree.contains(3L)).isEqualTo(false);
+
+        for (int i = 1; i < 100; i++) {
+            fileTree.add((long) 100 + i);
+            System.out.println("Adding: " + i);
+        }
+
+        System.out.println(fileTree.getHeight());
+        fileTree.reBalance();
+        System.out.println("Size: " + fileTree.getSize() + " Height: " + fileTree.getHeight());
+    }
+
+    private static Stream<Arguments> scenarios() throws IOException {
+        return Stream.of(
+                Arguments.of(createTree(0), 0),
+                Arguments.of(createTree(1), 1),
+                Arguments.of(createTree(2), 2),
+                Arguments.of(createTree(3), 2),
+                Arguments.of(createTree(4), 3),
+                Arguments.of(createTree(5), 3),
+                Arguments.of(createTree(6), 3),
+                Arguments.of(createTree(7), 3));
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("scenarios")
+    void testReBalance(FileTree fileTree, int expectedHeight) {
+        fileTree.reBalance();
+        assertThat(fileTree.getHeight()).isEqualTo(expectedHeight);
+    }
+
+    private static FileTree<Long> createTree(int elements) throws IOException {
+        Comparator<Long> comparator = new Comparator<Long>() {
+
+            @Override
+            public int compare(Long o1, Long o2) {
+                return Long.compare(o1, o2);
+            }
+        };
+
+        Converter<Long> converter = new Converter<Long>() {
+
+            @Override
+            public byte[] serialize(Long input) {
+                ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+                buffer.putLong(input);
+                return buffer.array();
+            }
+
+            @Override
+            public Long deserialize(byte[] input) {
+                ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+                buffer.put(input);
+                buffer.flip();//need flip
+                return buffer.getLong();
+            }
+
+            @Override
+            public int getSize() {
+                return Long.BYTES;
+            }
+        };
+
+        FileTree<Long> fileTree = new FileTree<>(File.createTempFile("___", ".tmp"), comparator, converter);
+
+        for (int i = 0; i < elements; i++) {
+            fileTree.add((long) i);
+        }
+
+        return fileTree;
+    }
+}
